8a6e5a9655cdf98a4a2acffc310f470dfdfb35e4
AlinNitu <alin.nitu1@gmail.com>
2021-01-18 00:04:17 +0000

    Update properties.ts

--- properties.ts
+++ properties.ts
@@ -1,9 +1,9 @@
 //these should be stored locally for better security
 
 const properties = {
-    baseUrl: 'https://app.3yourmind.com/en/accounts/login/',
-    validEmail: 'alin.nitu1@gmail.com',
-    validPassword: 'VeryNicePassword123',
+    baseUrl: 'https://someurl',
+    validEmail: 'validEmail',
+    validPassword: 'validPassword',
     validFormatModelFilePath: '../file-storage/Apple.stl'
 }
 
